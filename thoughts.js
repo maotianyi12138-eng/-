d9cda6e8a3f38bd914aff56891ff35666f2fda4c
maotianyi12138-eng <maotianyi12138@gmail.com>
2026-03-24 19:24:12 +0000

    Update thoughts.js

--- thoughts.js
+++ thoughts.js
@@ -1,17 +1,13 @@
-// thoughts.js
-// 以后每写一篇新的，就复制其中一个 { } 块到最前面即可
-
 const myThoughts = [
-  
     { 
         date: '2026.03.24', 
         tag: 'MOOD', 
         en: 'Discipline is choosing between what you want now and what you want most.', 
-        zh: '正在完善自己成为一个不无聊的人，电影里总是有桥段是意识到下半辈子还有和伴侣生活五六十年，就很绝望。我想不光是伴侣，家庭、朋友之间应该判断自己是否能为他们带去有趣、新鲜的事，因为当我们的身体和性功能退化后，我们只剩下大脑
+        zh: '正在完善自己成为一个不无聊的人。电影里总是有桥段：意识到下半辈子还要和伴侣生活五六十年，就很绝望。我想不光是伴侣，家庭、朋友之间也应判断自己是否能为他们带去有趣、新鲜的事。因为当我们的身体和感官退化后，我们最终只剩下灵魂的交流。'
     },
     { 
         date: '2026.03.24', 
-        tag: 'Mood', 
+        tag: 'MOOD', 
         en: 'I have been continuously improving the functionality, and finally at this point, I can write a diary thoughts here, and only those who care can see it', 
         zh: '一直完善功能，终于到了这一步可以写日记或者碎碎念了，而且只有在意的人能看到。' 
     }
