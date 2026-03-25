237d693d49aa9b976083a87b9906414f870bd614
maotianyi12138-eng <maotianyi12138@gmail.com>
2026-03-25 10:23:57 +0000

    Update thoughts.js

--- thoughts.js
+++ thoughts.js
@@ -9,12 +9,12 @@ const myThoughts = [
         date: '2026.03.24', 
         tag: 'MOOD', 
         en: '', 
-        zh: '正在完善自己成为一个不无聊的人。电影里总是有桥段：意识到下半辈子还要和伴侣生活五六十年，就很绝望。我想不光是伴侣，家庭、朋友之间也应判断自己是否能为他们带去有趣、新鲜的事。因为当我们的身体和感官退化后，我们最终只剩下灵魂的交流。'
+        zh: '我正在完善自己成为一个不无聊的人。电影里总是有情节：意识到下半辈子还要和伴侣生活五六十年，就很绝望。我觉得不光是伴侣，家庭、朋友之间也应判断自己是否能为他们带去有趣、新鲜的事。因为当我们的感受和性器官退化后，我们最终只剩下灵魂的交流。'
     },
     { 
         date: '2026.03.24', 
         tag: 'MOOD', 
         en: 'I have been continuously improving the functionality, and finally at this point, I can write a diary thoughts here, and only those who care can see it', 
-        zh: '一直完善功能，终于到了这一步可以写日记或者碎碎念了，而且只有在意的人能看到。' 
+        zh: '我一直完善功能，终于到了这一步可以写日记或者碎碎念了，而且只有在意的人能看到。' 
     }
 ];
